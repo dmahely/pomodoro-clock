e9419b7dc2f9ae327603ada7e883fb245c26692b
Doaa Mahely <dmahely@hotmail.com>
2020-08-01 17:09:23 +0400

    Change constants export

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,5 @@
 // default constant values
-const DEFAULT_SESSION_DURATION = 25;
-const DEFAULT_BREAK_DURATION = 5;
-const ALERT_SOUND =
+export const DEFAULT_SESSION_DURATION = 25;
+export const DEFAULT_BREAK_DURATION = 5;
+export const ALERT_SOUND =
   "https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3";
-
-  export { DEFAULT_SESSION_DURATION, DEFAULT_BREAK_DURATION, ALERT_SOUND };
