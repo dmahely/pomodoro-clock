f354644a90cace2e18af897d83030505143358f2
Doaa Mahely <dmahely@hotmail.com>
2020-08-04 01:07:24 +0400

    Updates

---
+++ precache-manifest.c2d0b9e443159f35b032528f05c2efe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0661b5c87a902c122f0c4eabd2079799",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d2e894e17c4658cc421e",
+    "url": "/pomodoro-clock/static/css/main.01ee0666.chunk.css"
+  },
+  {
+    "revision": "c2fdf65f7e32195f011e",
+    "url": "/pomodoro-clock/static/js/2.f0ba6a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.f0ba6a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e894e17c4658cc421e",
+    "url": "/pomodoro-clock/static/js/main.bfb623bd.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
